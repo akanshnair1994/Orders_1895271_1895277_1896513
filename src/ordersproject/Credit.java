8e7bd61b203d61f7918afcc7cc044d6303d5bc3b
1896513 <1896513@E21018.CEGEPGIM.LCL>
2019-02-10 09:18:02 -0500

    Creation of Class Credit with constructor, setter and getter methods.

---
+++ src/ordersproject/Credit.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ordersproject;
+
+import java.util.Date;
+
+/**
+ *
+ * @author 1896513
+ */
+public class Credit {
+
+    private String name;
+    private String type;
+    private Date expDate;
+
+    public Credit(String name, String type, Date expDate) {
+        this.name = name;
+        this.type = type;
+        this.expDate = expDate;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setexpDate(Date expDate) {
+        this.expDate = expDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public Date getexpDate() {
+        return expDate;
+    }
+
+    public void authorized() {
+        System.out.println("Authorize method");
+    }
+
+}
