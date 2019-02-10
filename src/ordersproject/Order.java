48f6a76b7936ab49e0073e3dea2efc55bee81467
1895271 <1895271@E21015.CEGEPGIM.LCL>
2019-02-10 08:54:26 -0500

    Created class Order with the constructor, getter/setter methods, and the other methods defined

---
+++ src/ordersproject/Order.java
@@ -0,0 +1,45 @@
+package ordersproject;
+
+import java.util.Date;
+
+public class Order {
+    Date date;
+    String status;
+    
+    public Order(Date date, String status) {
+        this.date = date;
+        this.status = status;
+    }
+    
+    public void calcSubTotal() {
+        System.out.println("This is the calcSubTotal() method");
+    }
+    
+    public void calcTax() {
+        System.out.println("This is the calcTax() method");
+    }
+    
+    public void calcTotal() {
+        System.out.println("This is the calcTotal() method");
+    }
+    
+    public void calcTotalWeight() {
+        System.out.println("This is the calcTotalWeight() method");
+    }
+    
+    public void setDate(Date date) {
+        this.date = date;
+    }
+    
+    public Date getDate() {
+        return date;
+    }
+    
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    
+    public String getStatus() {
+        return status;
+    }
+}
