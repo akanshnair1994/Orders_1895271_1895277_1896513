055a21f824cc1ccac97f9bb14c7f66513672bce6
1895271 <1895271@E21015.CEGEPGIM.LCL>
2019-02-10 09:18:10 -0500

    Created class Payment with the constructor and getter/setter methods

---
+++ src/ordersproject/Payment.java
@@ -0,0 +1,17 @@
+package ordersproject;
+//@author 1895271
+public class Payment {
+    private float amount;
+    
+    public Payment(float amount) {
+        this.amount = amount;
+    }
+    
+    public void setAmount(float amount) {
+        this.amount = amount;
+    }
+    
+    public float getAmount() {
+        return amount;
+    }
+}
