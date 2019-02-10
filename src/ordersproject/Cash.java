3721660bd7e09a22521128ed3061d711c3c70867
1896513 <1896513@E21018.CEGEPGIM.LCL>
2019-02-10 08:51:52 -0500

    Creation of Class Cash with constructor, setter and getter methods.

---
+++ src/ordersproject/Cash.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ordersproject;
+
+/**
+ *
+ * @author 1896513
+ */
+public class Cash {
+
+    float cashTendered;
+
+    public Cash(float cashTendered) {
+        this.cashTendered = cashTendered;
+    }
+
+    public void setcashTendered(float cashTendered) {
+        this.cashTendered = cashTendered;
+    }
+
+    public float getcashTendered() {
+        return cashTendered;
+    }
+
+}
